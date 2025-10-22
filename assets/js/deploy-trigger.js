da559b2b4c17f347b79da5fc79b43a0e566157a8
Jeff <417356479@qq.com>
2025-10-22 17:43:19 +0800

    feat: trigger deployment for enhanced video path testing

--- assets/js/deploy-trigger.js
+++ assets/js/deploy-trigger.js
@@ -1,1 +1,1 @@
-// Trigger deployment - 2025年10月22日 15:30:00 - Enhanced video path detection with multiple fallbacks
+// Trigger deployment - 2025年10月22日 15:45:00 - Testing enhanced video path detection
