c4bb3cbcc9166e0bc50dc6e5f0a72a19bd3b82e5
Jeff9420 <417356479@qq.com>
2025-11-15 14:58:36 +0800

    控制器

--- assets/js/index-app.js
+++ assets/js/index-app.js
@@ -470,7 +470,6 @@ function initializeApp() {
     loadCategories();
     initRecommendationRail();
     prefillInstantPlayer();
-    openPlayerModal(false);
 
     const heroStartBtn = document.querySelector('[data-action="start-default-session"]');
     if (heroStartBtn) {
